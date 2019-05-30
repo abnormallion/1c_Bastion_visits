5b71b24cfaa5d80d9e42371d27e916c8aa9a0ad9
abnormallion <in.petrov11@gmail.com>
2019-05-30 14:52:07 +0300

    Initial commit

---
+++ src/dbOperations/dbOperations.js
@@ -0,0 +1,225 @@
+const fs = require("fs");
+const async = require("async");
+const mssql = require("mssql");
+const moment = require("moment");
+
+const config = require("../config/config");
+
+var dateNow = moment(new Date()).format("YYYY_MM_DD");
+var path = config.pathFolderWithFilesBastion + dateNow;
+var dbconfig = config.dbconfig;
+
+function getVisitsBreakfast(workMode, result, callback) {
+  if (workMode.breakfast != undefined) {
+    timeStart = workMode.breakfast.periodWith;
+    timeFinish = workMode.breakfast.periodBy;
+    getRecords(timeStart, timeFinish, objectResult => {
+      console.log("breakfast");
+      if (objectResult.err) {
+        console.log(objectResult.err);
+        callback();
+      } else {
+        console.log(objectResult.recordsNumber);
+        result.breakfast.factVisit = objectResult.recordsNumber;
+        callback();
+      }
+    });
+  } else {
+    callback();
+  }
+}
+
+function getVisitsDinner(workMode, result, callback) {
+  if (workMode.dinner != undefined) {
+    timeStart = workMode.dinner.periodWith;
+    timeFinish = workMode.dinner.periodBy;
+    getRecords(timeStart, timeFinish, objectResult => {
+      console.log("dinner");
+      if (objectResult.err) {
+        console.log(objectResult.err);
+        callback();
+      } else {
+        console.log(objectResult.recordsNumber);
+        result.dinner.factVisit = objectResult.recordsNumber;
+        callback();
+      }
+    });
+  } else {
+    callback();
+  }
+}
+
+function getVisitsSupper(workMode, result, callback) {
+  if (workMode.supper != undefined) {
+    timeStart = workMode.supper.periodWith;
+    timeFinish = workMode.supper.periodBy;
+    getRecords(timeStart, timeFinish, objectResult => {
+      console.log("supper");
+      if (objectResult.err) {
+        console.log(objectResult.err);
+        callback();
+      } else {
+        console.log(objectResult.recordsNumber);
+        result.supper.factVisit = objectResult.recordsNumber;
+        callback();
+      }
+    });
+  } else {
+    callback();
+  }
+}
+
+var getVisitsFromDB = function(workMode, result, callback) {
+  getVisitsBreakfast(workMode, result, () => {
+    getVisitsDinner(workMode, result, () => {
+      getVisitsSupper(workMode, result, () => {
+        console.log("callback");
+        callback();
+      });
+    });
+  });
+};
+
+var addRecord = function(fileRecords, pathFile) {
+  var array = pathFile.split("/");
+  var fileName = "";
+  if (array.length > 0) fileName = array[array.length - 1];
+
+  async.each(
+    fileRecords,
+    (jsonData, callback) => {
+      var conn = new mssql.ConnectionPool(dbconfig);
+      conn
+        .connect()
+        .then(() => {
+          var request = new mssql.Request(conn);
+
+          field1Value = jsonData.field1;
+          periodValue = new Date(
+            moment(jsonData.period, "DD-MM-YYYY HH:mm:ss")
+          ).toISOString();
+          console.log(periodValue);
+          request.input("param1", field1Value);
+          request.input("param2", periodValue);
+
+          request
+            .query(
+              "SELECT * From TableTest2 Where period = @param2 AND CONVERT(VARCHAR, field1) = @param1"
+            )
+            .then(recordset => {
+              console.log(recordset.recordset.length);
+              if (recordset != undefined && recordset.recordset.length > 0) {
+                console.log("Don't insert row - this record yet exists");
+                callback();
+              } else {
+                console.log("Can insert row");
+                periodValue = new Date(
+                  moment(jsonData.period, "DD-MM-YYYY HH:mm:ss")
+                ).toISOString();
+                var textQuery =
+                  "Insert INTO TableTest2 (field1, period, field3, field4, field5, field6) VALUES ";
+                textQuery +=
+                  "('" +
+                  jsonData.field1 +
+                  "', '" +
+                  periodValue +
+                  "', '" +
+                  jsonData.field3 +
+                  "', '" +
+                  jsonData.field4 +
+                  "', '" +
+                  jsonData.field5 +
+                  "', '" +
+                  jsonData.field6 +
+                  "')";
+                console.log(textQuery);
+                request
+                  .query(textQuery)
+                  .then(recordset => {
+                    console.log("Success add new order to db");
+                    console.log(recordset);
+                    callback();
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    callback(err);
+                  });
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              callback(err);
+            });
+        })
+        .catch(err => {
+          console.log(err);
+          callback(err);
+        });
+    },
+    err => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        //rename file
+        console.log(pathFile);
+        fs.rename(new URL(pathFile), new URL(path + "/$$_" + fileName), err => {
+          if (err) throw err;
+          console.log("Rename complete!");
+        });
+      }
+    }
+  );
+};
+
+var getRecords = function(periodHoursWith, periodHoursBy, callback) {
+  // sql
+  //   .connect(config)
+  //   .then(() => {
+
+  var conn = new mssql.ConnectionPool(dbconfig);
+  conn
+    .connect()
+    .then(() => {
+      console.log("Connect to MSSQL");
+
+      // create Request object
+      var request = new mssql.Request(conn);
+      var dateNowFormatted = moment(new Date()).format("DD.MM.YYYY");
+
+      //var periodHoursWith = "15:00:00";
+      var periodWith = dateNowFormatted + " " + periodHoursWith;
+      var periodWithValue = new Date(
+        moment(periodWith, "DD-MM-YYYY HH:mm:ss")
+      ).toISOString();
+      //var periodHoursBy = "18:00:00";
+      var periodBy = dateNowFormatted + " " + periodHoursBy;
+      var periodByValue = new Date(
+        moment(periodBy, "DD-MM-YYYY HH:mm:ss")
+      ).toISOString();
+      console.log("Period with " + periodWithValue + " by " + periodByValue);
+      request.input("param1", periodWithValue);
+      request.input("param2", periodByValue);
+      request
+        .query(
+          "select * from TableTest2 WHERE period BETWEEN @param1 AND @param2"
+        )
+        .then(recordset => {
+          console.log(recordset.recordset.length);
+          console.log(recordset);
+          callback({ recordsNumber: recordset.recordset.length });
+          //sql.close();
+        })
+        .catch(err => {
+          console.log(err);
+          callback({ err });
+          //sql.close();
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      callback({ err });
+      // sql.close();
+    });
+};
+
+module.exports = { addRecord, getRecords, getVisitsFromDB };
---
+++ src/filesOperations/filesOperations.js
@@ -0,0 +1,119 @@
+const fs = require("fs");
+const moment = require("moment");
+
+const dbOperations = require("../dbOperations/dbOperations");
+const config = require("../config/config");
+
+var dateNow = moment(new Date()).format("YYYY_MM_DD");
+var path = config.pathFolderWithFilesBastion + dateNow;
+
+console.log(path);
+
+function getFile(filename) {
+  //console.log(filename);
+  return fs.readFileAsync(filename, "utf8");
+}
+
+function isDataFile(filename) {
+  return filename.substring(0, 2) !== "$$";
+}
+
+fs.readdirAsync = dirname => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirname, (err, filenames) => {
+      if (err) reject(err);
+      else resolve(filenames);
+    });
+  });
+};
+
+fs.readFileAsync = (filename, enc) => {
+  return new Promise((resolve, reject) => {
+    var fullFileName = path + "/" + filename;
+
+    fs.readFile(new URL(fullFileName), enc, (err, data) => {
+      if (err) reject(err);
+      else {
+        var object = {
+          data: data,
+          fileName: fullFileName
+        };
+        //console.log(object);
+        resolve(object);
+        // console.log(fullFileName);
+      }
+    });
+  });
+};
+
+var checkFiles = function() {
+  fs.readdirAsync(new URL(path))
+    .then(filenames => {
+      filenames = filenames.filter(isDataFile);
+      //console.log(filenames);
+      return Promise.all(filenames.map(getFile));
+    })
+    .then(files => {
+      //console.log(objectFiles);
+      //console.log(fileName);
+      files.forEach(objectFile => {
+        var file = objectFile.data;
+        var fileName = objectFile.fileName;
+
+        //console.log(file);
+
+        var fileRecords = [];
+        var arrayString = file.split("\r");
+        //console.log(arrayString);
+        arrayString.forEach(elementString => {
+          elementString = elementString.replace(/\r?\n/g, "");
+          var arrayElements = elementString.split(";");
+
+          //console.log(elementString);
+          var recordObject = {
+            field1: "",
+            period: "",
+            field3: "",
+            field4: "",
+            field5: "",
+            field6: ""
+          };
+
+          isPushed = false;
+          for (let i = 0; i < arrayElements.length; i++) {
+            var element = arrayElements[i];
+            if (element === "") continue;
+            isPushed = true;
+            if (i === 0) {
+              recordObject.field1 = element;
+            }
+            if (i === 1) {
+              recordObject.period = element;
+            }
+            if (i === 2) {
+              recordObject.field3 = element;
+            }
+            if (i === 3) {
+              recordObject.field4 = element;
+            }
+            if (i === 4) {
+              recordObject.field5 = element;
+            }
+            if (i === 5) {
+              recordObject.field6 = element;
+            }
+          }
+
+          if (isPushed) fileRecords.push(recordObject);
+        });
+        // console.log(fileRecords.length);
+        // console.log(fileName);
+        dbOperations.addRecord(fileRecords, fileName);
+      });
+    })
+    .catch(err => {
+      if (err) console.log(err);
+    });
+};
+
+module.exports = { checkFiles };
